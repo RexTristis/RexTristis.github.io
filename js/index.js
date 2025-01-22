d4243431194b306328c2d5757893a8723ea6373b
RexTristis <136982979+RexTristis@users.noreply.github.com>
2025-01-21 17:26:02 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const btnTwo = document.querySelector('#btn-two');
 const scriptId = '_helpia_ws_chat';
 const scriptSrc = 'https://statics.helpia.com/cgc/webchat/develop/widget.js';
 //const script = document.querySelector('#_helpia_ws_chat');
-
+console.log('cargador de widgets');
 const scriptInfo = [
     //Fenalco Presidencia.
     'eyJjb25maWdJZCI6InNib3RsbC0tNnducXJieSIsIndzVG9rZW4iOiJMUFBtVHlaWEFtUmsySEV3dER3bWsyaGZzQjFmQWI5MkFKdm5DRXhuNjFHQTciLCJiYWNrU2VydmVyIjoiaHR0cHM6Ly9kZXYuYmFja2VuZC5jZ2MuaGVscGlhLmNvbSIsIndzU2VydmVyIjoid3NzOi8vZGV2LndzLmJhY2tlbmQuY2djLmhlbHBpYS5jb20vY2hhbm5lbHMifQ==',
